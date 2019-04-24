dd09a67ca4b7a5d73b7dc7a7eb67cc8b128cb91d
aman mohsin <amanmohsin250@outlook.com>
2019-04-23 20:07:57 -0700

    Created a controller action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Details()
+        {
+            return "Hello...Welcome to the asp.net controller!";
+        }
     }
 }
